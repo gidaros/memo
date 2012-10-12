f1c7195f6f3d4b829321e4474d776c0c5854d7b3
Dimitrios Menounos <dmenounos@yahoo.com>
2012-10-13 02:52:11 +0300

    ...

---
+++ memo-portal/src/main/java/memo/web/portal/server/PortalController.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2012 Dimitrios Menounos
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package memo.web.portal.server;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PortalController {
+
+	@RequestMapping(value = "/home")
+	public ModelAndView doHome(HttpServletRequest request) {
+		return new ModelAndView("/home");
+	}
+
+	@RequestMapping(value = "/test")
+	public ModelAndView doTest(HttpServletRequest request) {
+		synchronized (this) {
+			try {
+				wait(3000);
+			}
+			catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		return new ModelAndView("/test");
+	}
+}
